586c529595df0e8b450a7595561ceebe935e2bf8
minirachel <rachel.a.levinson@gmail.com>
2022-02-21 15:02:07 -0500

    commit tree 73cdd7eb6331703dda4277b74e72a90c536b9554

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+let game = document.getElementById("game");
+let maxRight = game.clientWidth - dodger.clientWidth;
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers,10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+// radix is the base number. 10 converts a decimal into a whole
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers,10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
